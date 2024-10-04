bb63b4c3e5522d09f6693df53e945903890e0659
Randi Mays <rmays@kindsys.us>
2024-10-04 09:15:54 -0500

    First commit for Udemy course

---
+++ components/main-header.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import logoImg from '@/assets/logo.png';
+import styles from './main-header.module.css';
+
+export default function MainHeader() {
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        <Image
+          src={logoImg}
+          alt="A plate with food on it"
+          width={500}
+          height={500}
+          priority
+        />
+        NextLevel Food
+      </Link>
+
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
