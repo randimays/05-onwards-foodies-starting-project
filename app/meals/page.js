f2e4d70aa4d67073746d45289f031d45426920f7
Randi Mays <rmays@kindsys.us>
2024-10-15 10:23:45 -0500

    Adding meal slug page functionality and styles

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,20 +1,32 @@
 import Link from 'next/link';
 import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-export default function MealsPage() {
+const Meals = async () => {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+};
+
+export default async function MealsPage() {
+
   return (
     <>
       <header className={classes.header}>
-        <h1>Delicious meals created by <span className={classes.highlight}>by you</span></h1>
+        <h1>Delicious meals created <span className={classes.highlight}>by you</span></h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
+        </p>
       </header>
-      <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
-      <p className={classes.cta}>
-        <Link href="/meals/share">
-          Share Your Favorite Recipe
-        </Link>
-      </p>
       <main className={classes.main}>
-
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   )
